94e67d6f99abf70026ea87f779e501dfc6523ddf
Usmanwasim <94521662+usmanwasim@users.noreply.github.com>
2023-10-14 02:41:23 +0500

    Update bot.js

--- utils/bot.js
+++ utils/bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const apiToken = "6104050518:AAFZL6wRK-YQZ-7L8eEU3DMESlN6G9azLR4"; //development
-// const apiToken = "6645848236:AAFRkRvLUjbNM0qQjKbBY7B73mZFAX4luM4"; //production
+// const apiToken = "6104050518:AAFZL6wRK-YQZ-7L8eEU3DMESlN6G9azLR4"; //development
+const apiToken = "6633726543:AAHNatJ0T7hS0F3xGE6zuGqJ-iJl0bZC0LM"; //production
 
 const bot = new TelegramBot(apiToken, {
 	polling: true,
