99511df7e5fd0622f5322853a0e5ebab053ebfee
Usmanwasim <mianusmanwasim@gmail.com>
2024-03-12 23:53:19 +0500

    config_vercel:🚀

--- utils/bot.js
+++ utils/bot.js
@@ -1,10 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-// const apiToken = "6104050518:AAFZL6wRK-YQZ-7L8eEU3DMESlN6G9azLR4"; //development
-const apiToken = "6633726543:AAHNatJ0T7hS0F3xGE6zuGqJ-iJl0bZC0LM"; //production
-
-const bot = new TelegramBot(apiToken, {
-	polling: true,
+const bot = new TelegramBot(process.env.Bot_Token, {
+  polling: true,
 });
 
 module.exports.bot = bot;
